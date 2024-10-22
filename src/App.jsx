5dcf63f262a598488ec268ce2efa35ef640b9af3
Rahil <rahil.aliyev@kapitalbank.az>
2024-10-22 11:54:33 +0400

    adding fingerprint function

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FingerPrintComponent from "./components/Fingerprint";
+
+function App() {
+  return (
+    <>
+      <FingerPrintComponent />
+    </>
+  );
+}
+
+export default App;
